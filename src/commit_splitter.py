0fa12a00fd5279a899420a2f6878762727fe18ff
Patrick Ayoup <patrick.ayoup@gmail.com>
2017-03-02 23:41:55 -0500

    Implemented the Change class

--- src/commit_splitter.py
+++ src/commit_splitter.py
@@ -1,5 +1,27 @@
 class Change(object):
-	pass
+	
+	def __init__(self, operation, line, line_number, file_path):
+		"""
+		:param operation: The operation of this change.
+		:type operation: str
+		:param line: The contents of the line.
+		:type line: str
+		:param line_number: The line number of this change.
+		:type line_number: int
+		:param file_path: The path to the file containing this change.
+		:type file_path: str
+		"""
+
+		self.operation = operation
+		self.line = line
+		self.line_number = line_number
+		self.file_path = file_path
+
+	def __str__(self):
+		return f'({self.operation}, {self.line}, {self.line_number}, {self.file_path})'
+
+	def __repr__(self):
+		return str(self)
 
 
 def collect_changes(commit):
@@ -25,8 +47,6 @@ def collect_changes(commit):
 	and the file path.
 	"""
 
-	# TODO define a class for a change.
-
 	for parent in commit.parents:
 		for d in parent.diff(commit, create_patch=True):
 			diff = d.diff.decode('utf-8')
@@ -38,10 +58,10 @@ def collect_changes(commit):
 					old_line_number = int(info[0][1:].split(',')[0])
 					new_line_number = int(info[1][1:].split(',')[0])
 				elif change.startswith('-'):
-					changes.append(('del', change[1:], old_line_number, d.a_path))
+					changes.append(Change('del', change[1:], old_line_number, d.a_path))
 					old_line_number += 1
 				elif change.startswith('+'):
-					changes.append(('add', change[1:], new_line_number, d.b_path))
+					changes.append(Change('add', change[1:], new_line_number, d.b_path))
 					new_line_number += 1
 				elif change == '\\ No newline at end of file':
 					continue
